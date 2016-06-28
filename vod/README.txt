4056a7304ee5e0f9eaab9424ce6744bb6f54683c
Yerik Gao <yerik.gao@gmail.com>
2016-06-28 12:35:41 +0800

    add VOD&STS demo

---
+++ vod/README.txt
@@ -0,0 +1,17 @@
+本示例为通过STS授权方式访问VOD；
+
+STS服务可以生成一个临时的AK SK，这个临时的AK SK有有效期，从而保证安全性，多用于移动终端、web前端的方式访问BCE的服务；
+目前开放云的STS服务只提供了Java SDK，如果使用其他语言的开发者，只能参考官方的开源SDK以及REST API文档编写，STS服务的使用方式与BCE的大多数的服务类似，通过签名算法来实现；
+
+关于STS的详细介绍不再赘述，可以参考BCE文档：https://bce.baidu.com/doc/BOS/API.html#.E8.AE.BF.E9.97.AE.E6.8E.A7.E5.88.B6
+
+sts-v1.1-vod.php	获取STS服务生成的临时AK、SK以及sessionToken	
+vod-v1-sample.php	通过STS生成的验证信息访问VOD服务，演示查询某个媒资信息；
+
+使用方法：
+1、将sts-v1.1-vod.php中的AK、SK改成自己的BCE账号的AK、SK ，运行该文件可以获取一个STS服务生成的AK、SK以及sessionToken；
+注意：VOD对STS得到的ak/sk只提供新增媒资和获取单个媒资的权限，且不可设置，所以其中传递给STS服务的json body权限设置为空！
+
+2、修改vod-v1-sample.php文件中的AK、SK、sessionToke，值为第一步STS获取的AK、SK以及sessionToken；修改mediaId为自己创建的媒资ID；
+
+3、运行vod-v1-sample.php 文件，将返回自己的媒资信息；
---
+++ vod/vod-v1-sample.php
@@ -0,0 +1,197 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | PHP version 5                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2004 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 3.0 of the PHP license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.php.net/license/3_0.txt.                                  |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Authors: Original Author < yerik.gao@gmail.com >                     |
+// |          Your Name < Mr Gao >                                        |
+// +----------------------------------------------------------------------+
+//
+// $Id:$
+
+class HttpUtil {
+    // 根据RFC 3986，除了：
+    //   1.大小写英文字符
+    //   2.阿拉伯数字
+    //   3.点'.'、波浪线'~'、减号'-'以及下划线'_'
+    // 以外都要编码
+    public static $PERCENT_ENCODED_STRINGS;
+    //填充编码数组
+    public static function __init() {
+        HttpUtil::$PERCENT_ENCODED_STRINGS = array();
+        for ($i = 0; $i < 256; ++$i) {
+            HttpUtil::$PERCENT_ENCODED_STRINGS[$i] = sprintf("%%%02X", $i);
+        }
+        //a-z不编码
+        foreach (range('a', 'z') as $ch) {
+            HttpUtil::$PERCENT_ENCODED_STRINGS[ord($ch) ] = $ch;
+        }
+        //A-Z不编码
+        foreach (range('A', 'Z') as $ch) {
+            HttpUtil::$PERCENT_ENCODED_STRINGS[ord($ch) ] = $ch;
+        }
+        //0-9不编码
+        foreach (range('0', '9') as $ch) {
+            HttpUtil::$PERCENT_ENCODED_STRINGS[ord($ch) ] = $ch;
+        }
+        //以下4个字符不编码
+        HttpUtil::$PERCENT_ENCODED_STRINGS[ord('-') ] = '-';
+        HttpUtil::$PERCENT_ENCODED_STRINGS[ord('.') ] = '.';
+        HttpUtil::$PERCENT_ENCODED_STRINGS[ord('_') ] = '_';
+        HttpUtil::$PERCENT_ENCODED_STRINGS[ord('~') ] = '~';
+    }
+    //在uri编码中不能对'/'编码
+    public static function urlEncodeExceptSlash($path) {
+        return str_replace("%2F", "/", HttpUtil::urlEncode($path));
+    }
+    //使用编码数组编码
+    public static function urlEncode($value) {
+        $result = '';
+        for ($i = 0; $i < strlen($value); ++$i) {
+            $result.= HttpUtil::$PERCENT_ENCODED_STRINGS[ord($value[$i]) ];
+        }
+        return $result;
+    }
+    //生成标准化QueryString
+    public static function getCanonicalQueryString(array $parameters) {
+        //没有参数，直接返回空串
+        if (count($parameters) == 0) {
+            return '';
+        }
+        $parameterStrings = array();
+        foreach ($parameters as $k => $v) {
+            //跳过Authorization字段
+            if (strcasecmp('Authorization', $k) == 0) {
+                continue;
+            }
+            if (!isset($k)) {
+                throw new InvalidArgumentException("parameter key should not be null");
+            }
+            if (isset($v)) {
+                //对于有值的，编码后放在=号两边
+                $parameterStrings[] = HttpUtil::urlEncode($k) . '=' . HttpUtil::urlEncode((string)$v);
+            } else {
+                //对于没有值的，只将key编码后放在=号的左边，右边留空
+                $parameterStrings[] = HttpUtil::urlEncode($k) . '=';
+            }
+        }
+        //按照字典序排序
+        sort($parameterStrings);
+        //使用'&'符号连接它们
+        return implode('&', $parameterStrings);
+    }
+    //生成标准化uri
+    public static function getCanonicalURIPath($path) {
+        //空路径设置为'/'
+        if (empty($path)) {
+            return '/';
+        } else {
+            //所有的uri必须以'/'开头
+            if ($path[0] == '/') {
+                return HttpUtil::urlEncodeExceptSlash($path);
+            } else {
+                return '/' . HttpUtil::urlEncodeExceptSlash($path);
+            }
+        }
+    }
+    //生成标准化http请求头串
+    public static function getCanonicalHeaders($headers) {
+        //如果没有headers，则返回空串
+        if (count($headers) == 0) {
+            return '';
+        }
+        $headerStrings = array();
+        foreach ($headers as $k => $v) {
+            //跳过key为null的
+            if ($k === null) {
+                continue;
+            }
+            //如果value为null，则赋值为空串
+            if ($v === null) {
+                $v = '';
+            }
+            //trim后再encode，之后使用':'号连接起来
+            $headerStrings[] = HttpUtil::urlEncode(strtolower(trim($k))) . ':' . HttpUtil::urlEncode(trim($v));
+        }
+        //字典序排序
+        sort($headerStrings);
+        //用'\n'把它们连接起来
+        return implode("\n", $headerStrings);
+    }
+}
+HttpUtil::__init();
+function getSigner($host,$httpMethod, $path, $parms, $timestamp) {
+    //	date_default_timezone_set('UTC');
+    $AK = "f7306cacee2942c98a715ddc0d5b3878";    //替换成你STS获取的AK
+    $SK = "2cf0173d82624502bbd7150c03b5b236";    //替换成你STS获取的SK
+    $expirationPeriodInSeconds = "3600";
+    //$timestamp = date("Y-m-d")."T".date("H:i:s")."Z";
+    $authStringPrefix = "bce-auth-v1" . "/" . $AK . "/" . $timestamp . "/" . $expirationPeriodInSeconds;
+    $SigningKey = hash_hmac('SHA256', $authStringPrefix, $SK);
+    $CanonicalHeaders1 = "host;" . "x-bce-date";
+    $CanonicalHeaders2 = "host:".$host."\n" . "x-bce-date:" . urlencode($timestamp); //
+    //print "CanonicalHeaders2:" . $CanonicalHeaders2 . "\n";
+    $httputil = new HttpUtil();
+    $CanonicalString = $httputil->getCanonicalQueryString($parms);
+    $CanonicalURI = $path;
+    $Method = $httpMethod;
+    $CanonicalRequest = $Method . "\n" . $CanonicalURI . "\n" . $CanonicalString . "\n" . $CanonicalHeaders2;
+    //print "CanonicalRequest:" . $CanonicalRequest . "\n";
+    $Signature = hash_hmac('SHA256', $CanonicalRequest, $SigningKey);
+    //print "Signature:" . $Signature . "\n";
+    $Authorization = "bce-auth-v1/{$AK}/" . $timestamp . "/{$expirationPeriodInSeconds}/{$CanonicalHeaders1}/{$Signature}";
+    print "Authorization:" . $Authorization . "\n";
+    return $Authorization;
+    //$Authorization:
+    //bce-auth-v1/97659fdf3ab547e2a9f71dfcd6659a8b2015-08-27T03:50:33Z/3600/content-length;host;x-bce-date;/5290e5669befd7f44dd362e00c2a4cc5edbee0bc925bceb15e8a9fcb91389201
+    
+}
+
+//查询媒资信息示例
+
+$mediaId = "mda-gfyhginvv3b00rpz";  //替换成你的媒资ID
+$host= "vod.baidubce.com";
+$path = "/v1/media/".$mediaId;
+$url = "http://".$host.$path;
+$method = "GET";
+$parms = array();
+
+//替换成STS获取的sessionToken
+$stsSessionToken = "MjUzZjQzNTY4OTE0NDRkNjg3N2E4YzJhZTc4YmU5ZDh8AAAAADgBAADDwfoCSQ/6VjiLiZ4+S7xAC9LXn2EVF4EEHyEI6Jys8K7jYe+ov0SzR2WRXl5CFNy+EfBUFW2+QpDyzGRTPddluKjZacQF6x3wPJm2s5lj4ZvdVmZcdOPLUVG2fft4zLUDW9IEUqEwinHQhmRFVcC2xagba2AvKMh+Wmcx3T+3Iu/nWPF2agtRIdfy/7NcrpXcb/DbDqqqxPzX1px1q/mbGowAzhnq+plj+g9oKOfdnRLCs6JDrX4XPtSlg9uOE1c=";
+
+date_default_timezone_set('UTC');
+$timestamp = date("Y-m-d") . "T" . date("H:i:s") . "Z";
+$Authorization = getSigner($host,$method,$path, $parms, $timestamp);
+
+
+$data = array();
+
+$data_string = json_encode($data);
+$head = array(
+    "Content-Type:text/plain",
+    "Content-Length:{$filesize}",
+    "Authorization:{$Authorization}",
+    "x-bce-date:{$timestamp}",
+    "x-bce-security-token:{$stsSessionToken}" //通过STS授权的方式访问VOD时，需要将sts的sessionToken传入header;
+);
+$curlp = curl_init();
+curl_setopt($curlp, CURLOPT_URL, $url);
+curl_setopt($curlp, CURLOPT_HTTPHEADER, $head);
+curl_setopt($curlp, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($curlp, CURLOPT_POST, 0);
+curl_setopt($curlp, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($curlp);
+curl_close($curlp);
+echo $output;
+print "\n";
+?>
